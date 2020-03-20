546fd142d4d150567b40e59d7f063d835866d345
graycebond <61305080+graycebond@users.noreply.github.com>
2020-03-20 15:07:01 +0800

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -1,4 +1,3 @@
-
 <footer class="container-fluid">
       
             <div class="container">
@@ -7,14 +6,14 @@
                     
                 <div class="col-md-6 text-center footer-content">
                     <p><?php bloginfo('name'); ?></p>
-                    <img alt="Tumblr" src="Images/Tumblr.jpg">
-                    <img alt="RSS" src="Images/RSS.jpg">
+                    <img alt="Tumblr" src="http://206.189.45.97/~meshatmurdoch3/wp-content/themes/starter_for_josh_site/images/tumblr.jpg">
+                    <img alt="RSS" src="http://206.189.45.97/~meshatmurdoch3/wp-content/themes/starter_for_josh_site/images/rss.jpg">
                 </div>
                     
                 <div class="col-md-6 text-center footer-content">
                     <p>Terms and Conditions</p>
-                    <img alt="Linkedin" src="Images/Linkedin.jpg">
-                    <img alt="Facebook" src="Images/Facebook.jpg">
+                    <img alt="Linkedin" src="http://206.189.45.97/~meshatmurdoch3/wp-content/themes/starter_for_josh_site/images/linkedin.jpg">
+                    <img alt="Facebook" src="http://206.189.45.97/~meshatmurdoch3/wp-content/themes/starter_for_josh_site/images/facebook.jpg">
                 </div>
                 
                 
