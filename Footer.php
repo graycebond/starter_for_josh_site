ae3cb56e6e9047fdc32c05e7b7d0783f38cf9a3b
graycebond <61305080+graycebond@users.noreply.github.com>
2020-03-20 13:35:09 +0800

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,25 @@
+
+<footer class="container-fluid">
+      
+            <div class="container">
+                
+                <div class="row">
+                    
+                <div class="col-md-6 text-center footer-content">
+                    <p><?php bloginfo('name'); ?></p>
+                    <img alt="Tumblr" src="Images/Tumblr.jpg">
+                    <img alt="RSS" src="Images/RSS.jpg">
+                </div>
+                    
+                <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p>
+                    <img alt="Linkedin" src="Images/Linkedin.jpg">
+                    <img alt="Facebook" src="Images/Facebook.jpg">
+                </div>
+                
+                
+                </div>
+          </div>
+      </footer>
+      
+<?php wp_footer(); ?>
