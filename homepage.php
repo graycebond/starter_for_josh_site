93204890527cc6bd1d2393e48e95685b0d8b58d3
graycebond <61305080+graycebond@users.noreply.github.com>
2020-04-25 15:00:04 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -7,7 +7,11 @@
       
       
       <section class="container-fluid aboutbg text-center">
-      
+       <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+<p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+
+          
           <div class="container">
           
   
